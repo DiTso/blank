97d872ff6cd07194c2e07c01c2e41a981e21615a
mnoskov <nm@perm-tools.ru>
2017-07-31 12:38:06 +0500

    Update setup.data.sql

--- install/setup.data.sql
+++ install/setup.data.sql
@@ -32,8 +32,6 @@ UPDATE `{PREFIX}system_settings` SET setting_value = '2' WHERE setting_name = 'd
 
 UPDATE `{PREFIX}system_settings` SET setting_value = 'sibling' WHERE setting_name = 'auto_template_logic';
 
-UPDATE `{PREFIX}system_settings` SET setting_value = 'MODxRE2_DropdownMenu' WHERE setting_name = 'manager_theme';
-
 UPDATE `{PREFIX}system_settings` SET setting_value = '/' WHERE setting_name = 'friendly_url_suffix';
 
 UPDATE `{PREFIX}system_settings` SET setting_value = 'YYYY-mm-dd' WHERE setting_name = 'datetime_format';
