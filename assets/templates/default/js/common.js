218f8ff57a678e620cb1657fb02bf52d974edf45
mnoskov <nm@perm-tools.ru>
2017-08-23 14:52:27 +0500

    $.cookie added

--- assets/templates/default/js/common.js
+++ assets/templates/default/js/common.js
@@ -46,6 +46,8 @@
 
 	$.fn.reachGoal=function(a){return this.each(function(){var n="undefined"==typeof a?$(this).data("goal"):a;if(window.Ya&&n)for(var i in window.Ya._metrika.counters)i="yaCounter"+i.replace(/:\d+$/,""),window[i]&&window[i].reachGoal(n)}),this};
 
+	$.cookie=function(e,i,o){if("undefined"==typeof i){var n=null;if(document.cookie&&""!=document.cookie)for(var r=document.cookie.split(";"),t=0;t<r.length;t++){var p=$.trim(r[t]);if(p.substring(0,e.length+1)==e+"="){n=decodeURIComponent(p.substring(e.length+1));break}}return n}o=o||{},null===i&&(i="",o.expires=-1);var u="";if(o.expires&&("number"==typeof o.expires||o.expires.toUTCString)){var s;"number"==typeof o.expires?(s=new Date,s.setTime(s.getTime()+24*o.expires*60*60*1e3)):s=o.expires,u="; expires="+s.toUTCString()}var a=o.path?"; path="+o.path:"",c=o.domain?"; domain="+o.domain:"",m=o.secure?"; secure":"";document.cookie=[e,"=",encodeURIComponent(i),u,a,c,m].join("")};
+	
 	/* Modernizr 2.8.3 (Custom Build) | MIT & BSD
 	 * Build: http://modernizr.com/download/#-csstransforms-teststyles-testprop-testallprops-prefixes-domprefixes
 	 */
