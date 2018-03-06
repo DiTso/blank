9c57b5319d57fa2aea0e3cee1fc5fbbb5fb444f8
mnoskov <nm@perm-tools.ru>
2018-03-06 15:27:57 +0500

    ebaniy updater should be off

--- install/setup.data.sql
+++ install/setup.data.sql
@@ -51,6 +51,9 @@ UPDATE `{PREFIX}system_settings` SET setting_value = 'advlist autolink lists lin
 
 UPDATE `{PREFIX}site_plugins` SET disabled = '0' WHERE name = 'ElementsInTree';
 
+UPDATE `{PREFIX}site_plugins` SET disabled = '0' WHERE name = 'OutdatedExtrasCheck';
+UPDATE `{PREFIX}site_plugins` SET disabled = '0' WHERE name = 'Updater';
+
 
 
 
