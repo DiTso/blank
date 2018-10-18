4547a4f331dea2037adf76d11221bba64e08ac2d
mnoskov <nm@perm-tools.ru>
2018-10-18 10:25:41 +0500

    default values for socials deleted

--- assets/modules/clientsettings/config/tab20.php
+++ assets/modules/clientsettings/config/tab20.php
@@ -7,27 +7,22 @@ return [
         'social_facebook' => [
             'caption' => 'Facebook',
             'type'  => 'text',
-            'default_text' => '#',
         ],
         'social_vkontakte' => [
             'caption' => 'ВКонтакте',
             'type'  => 'text',
-            'default_text' => '#',
         ],
         'social_instagram' => [
             'caption' => 'Instagram',
             'type'  => 'text',
-            'default_text' => '#',
         ],
         'social_youtube' => [
             'caption' => 'Youtube',
             'type'  => 'text',
-            'default_text' => '#',
         ],
         'social_odnoklassniki' => [
             'caption' => 'Одноклассники',
             'type'  => 'text',
-            'default_text' => '#',
         ],
     ],
 ];
